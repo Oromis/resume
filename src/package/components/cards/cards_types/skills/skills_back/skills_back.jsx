b87251ad416e668ea0e6d47916f05e9003d288f2
David Bauske <david.bauske@voltstorage.com>
2020-05-08 10:31:13 +0200

    Changed skills display to show bar charts only

--- src/package/components/cards/cards_types/skills/skills_back/skills_back.jsx
+++ src/package/components/cards/cards_types/skills/skills_back/skills_back.jsx
@@ -5,6 +5,7 @@ import { createUseStyles } from 'react-jss';
 import { config, useSpring, useChain } from 'react-spring';
 
 import { ProfileCardTitle } from '../../../../commons/profile_card/profile_card_title/profile_card_title';
+import SkillsBarChart from './skills_bar_chart/skills_bar_chart';
 
 import SkillsPieChart from './skills_pie_chart/skills_pie_chart';
 import OtherSkills from './other_skills/other_skills';
@@ -29,75 +30,15 @@ const Content = ({ data, handleAddButtonClick }) => {
     const [variant] = useCardVariant();
 
     const classes = useStyles({ variant });
-    const springSkillOpacityPropsRef = useRef();
-    const springGraphOpacityPropsRef = useRef();
-
     const hasSkill = useMemo(() => existsAndNotEmpty(data?.skills), [data]);
-
-    const springSkillOpacityProps = useSpring({
-        from: { opacity: 0 },
-        to: { opacity: 1 },
-        ref: springSkillOpacityPropsRef
-    });
-    const springGraphOpacityProps = useSpring({
-        from: { opacity: 0 },
-        to: { opacity: 1 },
-        ref: springGraphOpacityPropsRef
-    });
-
-    useChain([springGraphOpacityPropsRef, springSkillOpacityPropsRef], [0, 0.1]);
-
-    const [springOnScrollOpacityProps, setSpringOnScrollOpacityProps] = useSpring(() => ({
-        opacity: 1
-    }));
-    const [springTranslationProps, setSpringTranslationProps] = useSpring(() => ({ yt: 0, config: config.slow }));
-
-    const { top3Skills, othersSkills } = useMemo(() => {
-        const newData = [...(data.skills ?? [])];
-        const top3 = newData.splice(0, 3);
-        return {
-            top3Skills: top3,
-            othersSkills: newData
-        };
-    }, [data]);
-
-    const onScroll = useCallback(
-        (e) => {
-            const newOpacity = Math.max(1 - e.target.scrollTop / 60, 0);
-
-            if (newOpacity === 0) {
-                if (othersSkills.length > 10) {
-                    setSpringTranslationProps({ yt: -100 });
-                } else {
-                    setSpringTranslationProps({ yt: -100 + (e.target.scrollTop > 160 && e.target.scrollTop - 160) });
-                }
-            } else {
-                setSpringTranslationProps({ yt: 0 });
-            }
-
-            return setSpringOnScrollOpacityProps({ opacity: newOpacity });
-        },
-        [othersSkills]
-    );
 
     if (!hasSkill) {
         return <NoSkill {...{ handleAddButtonClick }} />;
     }
 
     return (
-        <div className={classes.container} onScroll={onScroll} style={springGraphOpacityProps}>
-            <SkillsPieChart
-                variant={variant}
-                data={top3Skills}
-                springOnScrollOpacityProps={springOnScrollOpacityProps}
-            />
-            {othersSkills.length > 1 && (
-                <OtherSkills
-                    style={springSkillOpacityProps}
-                    othersSkills={othersSkills}
-                    springTranslationProps={springTranslationProps}
-                />
-            )}
+        <div className={classes.container}>
+            <SkillsBarChart variant={variant} data={data.skills ?? []} />
         </div>
     );
 };
---
+++ src/package/components/cards/cards_types/skills/skills_back/skills_bar_chart/skills_bar_chart.jsx
@@ -0,0 +1,95 @@
+import chroma from 'chroma-js';
+import React, { useContext, useMemo } from 'react';
+import { createUseStyles, useTheme } from 'react-jss';
+import { animated } from 'react-spring';
+import { Bar, BarChart, Cell, LabelList, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { DEFAULT_TECHNO_HANDLE } from '../../../../../../utils/icons';
+import { getColorsFromCardVariant, getHexFromPaletteColor } from '../../../../../../utils/styles/styles_utils';
+import { useTechnologies } from '../../../../../hooks/technologies/use_technologies';
+import { styles } from './skills_bar_chart_styles';
+
+const marginSize = 8;
+const iconSize = 24;
+const margin = { top: marginSize, right: marginSize + iconSize, bottom: marginSize * 2, left: marginSize };
+
+const ColorContext = React.createContext({});
+const ColorProvider = ColorContext.Provider;
+
+function IconLabel(props) {
+    const { x, y, width, height, name } = props;
+    const { technologies } = useTechnologies();
+    const { theme, variantColors } = useContext(ColorContext);
+
+    const techno = useMemo(() => {
+        if (!technologies) {
+            return null;
+        }
+        return technologies[name];
+    }, [technologies, name]);
+
+    const src = useMemo(() => {
+        const hex = getHexFromPaletteColor(theme, variantColors.backgroundColor);
+        const luminance = chroma(hex).luminance();
+        if (luminance < 0.98) {
+            return `https://process.filestackapi.com/output=format:png/negative/modulate=brightness:1000/compress/${
+                techno?.handle || DEFAULT_TECHNO_HANDLE
+            }`;
+        }
+        return `https://process.filestackapi.com/output=format:png/${techno?.handle || DEFAULT_TECHNO_HANDLE}`;
+    }, [techno, theme, variantColors]);
+
+    return (
+        <image
+            x={x + width + marginSize}
+            y={y + height / 2 - iconSize / 2}
+            width={iconSize}
+            height={iconSize}
+            dy={0}
+            href={src}
+        />
+    );
+}
+
+const useStyles = createUseStyles(styles);
+const SkillsBarChart = ({ data, variant }) => {
+    const classes = useStyles();
+    const theme = useTheme();
+
+    const { contentColor, backgroundColor, variantColors } = useMemo(() => {
+        const variantColors = getColorsFromCardVariant(theme, variant);
+        return {
+            contentColor: getHexFromPaletteColor(theme, variantColors.color),
+            backgroundColor: getHexFromPaletteColor(theme, variantColors.backgroundColor),
+            variantColors
+        };
+    }, [theme, variant]);
+    const colorPalette = useMemo(
+        () => Array.from({ length: data.length }, (v, k) => chroma.mix(contentColor, backgroundColor, k / 15).hex()),
+        [contentColor, backgroundColor]
+    );
+
+    return (
+        <ColorProvider value={{ backgroundColor, theme, variantColors }}>
+            <animated.div className={classes.wrapper}>
+                <ResponsiveContainer height="100%" width="100%">
+                    <BarChart data={data} layout="vertical" margin={margin} maxBarSize={8}>
+                        <YAxis
+                            dataKey="name"
+                            type="category"
+                            axisLine={false}
+                            tickLine={false}
+                            width={100}
+                            stroke={contentColor}
+                        />
+                        <XAxis type="number" hide />
+                        <Bar dataKey="value" fill={contentColor}>
+                            <LabelList dataKey="name" content={IconLabel} position="insideEnd" />
+                        </Bar>
+                    </BarChart>
+                </ResponsiveContainer>
+            </animated.div>
+        </ColorProvider>
+    );
+};
+
+export default SkillsBarChart;
